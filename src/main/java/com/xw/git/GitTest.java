a36ccdf8f95ef5171fce86bb104a61893372badc
wspyyds <104574276+wspyyds@users.noreply.github.com>
2022-04-29 10:38:45 +0800

    pull commit

--- src/main/java/com/xw/git/GitTest.java
+++ src/main/java/com/xw/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master tset!");
         System.out.println("hello git hot-fix2!");
         System.out.println("push test!!");
+        System.out.println("pull test!!");
 
     }
 
